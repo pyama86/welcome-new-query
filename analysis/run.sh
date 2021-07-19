36a5b192a5ebc83886c5ba1e3e516f47706df040
pyama <pyama@pepabo.com>
2021-07-19 16:11:41 +0900

    yeay

--- analysis/run.sh
+++ analysis/run.sh
@@ -12,7 +12,7 @@ test -e ${OUTPUT_PATH} && cp ${OUTPUT_PATH} ${OUTPUT_PATH}_base
 
 query="select replace(replace(replace(replace(argument, '\r\n', ''), '\r', ''), '\n', ''), '\0', '') from general_log where command_type = 'Query';"
 `mysql -h ${DB_HOST} -u${DB_USER} ${PASSWORD} -Dmysql -sse "${query}"| \
-  sed -e 's/$/;/g' | pt-fingerprint | sort | uniq > ${OUTPUT_PATH}`
+  sed -e 's/$/;/g' | pt-fingerprint --match-embedded-numbers | sort | uniq > ${OUTPUT_PATH}`
 
 
 if [ `test -e ${OUTPUT_PATH}_base` ]; then
