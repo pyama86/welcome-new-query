b025f14a14d400d5f91ad4a3f0eaa3325a480d26
pyama <pyama@pepabo.com>
2021-07-19 15:36:21 +0900

    input parameter

--- analysis/run.sh
+++ analysis/run.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -x
-OUTPUT_PATH=$1
+DB_HOST=$1
+DB_USER=$2
+DB_PASSWORD=$3
+OUTPUT_PATH=$4
 
-MYSQL_USER=${MYSQL_USER:-root}
-MYSQL_HOST=${MYSQL_HOST:-localhost}
 
 test -e ${OUTPUT_PATH} && cp ${OUTPUT_PATH} ${OUTPUT_PATH}_base
-[ -v MYSQL_PASSWORD ] && PASSWORD="-p${MYSQL_PASSWORD}"
-[ "${MYSQL_USER}" = "root" ] && [ -v MYSQL_ROOT_PASSWORD ] && PASSWORD="-p${MYSQL_ROOT_PASSWORD}"
+
+[ -n "$DB_PASSWORD" ] && PASSWORD="-p${DB_PASSWORD}"
 
 query="select replace(replace(replace(replace(argument, '\r\n', ''), '\r', ''), '\n', ''), '\0', '') from general_log where command_type = 'Query';"
-`mysql -h ${MYSQL_HOST} -u${MYSQL_USER} ${PASSWORD} -Dmysql -sse "${query}"| \
+`mysql -h ${DB_HOST} -u${DB_USER} ${PASSWORD} -Dmysql -sse "${query}"| \
   sed -e 's/$/;/g' | pt-fingerprint | sort | uniq > ${OUTPUT_PATH}`
 
 
--- enable-querylog/run.sh
+++ enable-querylog/run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-MYSQL_USER=${MYSQL_USER:-root}
-MYSQL_HOST=${MYSQL_HOST:-localhost}
 
-[ -v MYSQL_PASSWORD ] && PASSWORD="-p${MYSQL_PASSWORD}"
-[ "${MYSQL_USER}" = "root" ] && [ -v MYSQL_ROOT_PASSWORD ] && PASSWORD="-p${MYSQL_ROOT_PASSWORD}"
+DB_HOST=$1
+DB_USER=$2
+DB_PASSWORD=$3
+OUTPUT_PATH=$4
+
+[ -n "$DB_PASSWORD" ] && PASSWORD="-p${DB_PASSWORD}"
 
 query="set global log_output = 'TABLE'";
-`mysql -h ${MYSQL_HOST} -u${MYSQL_USER} ${PASSWORD} -Dmysql -sse "${query}"`
+`mysql -h ${DB_HOST} -u${DB_USER} ${PASSWORD} -Dmysql -sse "${query}"`
 
 query="set global general_log = 1";
-`mysql -h ${MYSQL_HOST} -u${MYSQL_USER} ${PASSWORD} -Dmysql -sse "${query}"`
+`mysql -h ${DB_HOST} -u${DB_USER} ${PASSWORD} -Dmysql -sse "${query}"`
