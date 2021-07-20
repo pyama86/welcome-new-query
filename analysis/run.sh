5268c4b3b4e87f4010c04809f019eb5d5396ec2b
pyama <pyama@pepabo.com>
2021-07-20 11:06:42 +0900

    support s3

--- analysis/run.sh
+++ analysis/run.sh
@@ -4,6 +4,20 @@ DB_HOST=$1
 DB_USER=$2
 DB_PASSWORD=$3
 OUTPUT_PATH=$4
+export AWS_ACCESS_KEY_ID=$5
+export AWS_SECRET_ACCESS_KEY=$6
+export AWS_REGION=$7
+S3_BUCKET=$8
+
+FILENAME=`basename $OUTPUT_PATH`
+
+[ -n "$AWS_ACCESS_KEY_ID" ] && \
+[ -n "$AWS_SECRET_ACCESS_KEY" ] && \
+[ -n "$AWS_REGION" ] && \
+[ -n "$S3_BUCKET" ] {
+  USE_S3=1
+  aws s3 cp s3://$S3_BUCKET/$FILENAME ./  --quiet
+}
 
 
 test -e ${OUTPUT_PATH} && cp ${OUTPUT_PATH} ${OUTPUT_PATH}_base
@@ -20,6 +34,10 @@ if [ -e ${OUTPUT_PATH}_base ]; then
 else
   d=`cat ${OUTPUT_PATH}`
 fi
+
+[ -n "$USE_S3" ] {
+  aws s3 cp ${OUTPUT_PATH} s3://$S3_BUCKET/$FILENAME  --quiet
+}
 
 d="${d//$'\n'/\\n}"
 echo "::set-output name=new-queries::${d}"
